82f89b2ae030810803aadce3aec6eb6567ae0277
Sebosun <sebos@protonmail.com>
2021-03-30 21:52:20 +0200

    firebase setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Thread.css";
+import './Header.css'
 
 function Header(props) {
   return (
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { firebase } from "@firebase/app";
+require("firebase/firestore");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA9ApQ3Mhbga323RYlXqaW8X73QWtvprAg",
+    authDomain: "bes-chan.firebaseapp.com",
+    projectId: "bes-chan",
+    storageBucket: "bes-chan.appspot.com",
+    messagingSenderId: "889845667798",
+    appId: "1:889845667798:web:e97a653a43abfcf0963677",
+    measurementId: "G-MHY6F8BPC5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
