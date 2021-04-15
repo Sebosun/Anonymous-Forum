ab716bf4bc00ccc9b15c07d722d5f7c443c3d0c5
Sebosun <sebos@protonmail.com>
2021-04-15 16:38:38 +0200

    uploading images to sTORage works

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,17 +1,35 @@
 import React, { useState, useEffect } from "react";
 import "./PostForm.css";
+import { firebase } from "@firebase/app";
 
 function PostForm(props) {
   // TODO text verification, atm you can post infinite amount of text and it looks shit, not to mention spammy
   const [name, setName] = useState("");
   const [text, setText] = useState("");
   const [title, setTitle] = useState("");
+  const [image, setImage] = useState(null);
+  const [fileUrl, setFileUrl] = useState(null);
+
+  const onFileChange = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const onImageSubmit = async () => {
+    const file = image;
+    const storageRef = firebase.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileUrl(await fileRef.getDownloadURL());
+  };
+
   return (
     <div className="Form">
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          props.addNew(name, text, title);
+          onImageSubmit().then(console.log(fileUrl));
+          console.log(image);
+          // props.addNew(name, text, title, image);
         }}
         className="addForm"
       >
@@ -39,6 +57,8 @@ function PostForm(props) {
             setText(e.target.value);
           }}
         />
+
+        <input type="file" onChange={onFileChange} />
         <button>Submit</button>
       </form>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { firebase } from "@firebase/app";
 require("firebase/firestore");
+require("firebase/storage");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA9ApQ3Mhbga323RYlXqaW8X73QWtvprAg",
-    authDomain: "bes-chan.firebaseapp.com",
-    projectId: "bes-chan",
-    storageBucket: "bes-chan.appspot.com",
-    messagingSenderId: "889845667798",
-    appId: "1:889845667798:web:e97a653a43abfcf0963677",
-    measurementId: "G-MHY6F8BPC5"
+  apiKey: "AIzaSyA9ApQ3Mhbga323RYlXqaW8X73QWtvprAg",
+  authDomain: "bes-chan.firebaseapp.com",
+  projectId: "bes-chan",
+  storageBucket: "bes-chan.appspot.com",
+  messagingSenderId: "889845667798",
+  appId: "1:889845667798:web:e97a653a43abfcf0963677",
+  measurementId: "G-MHY6F8BPC5",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -22,7 +23,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
