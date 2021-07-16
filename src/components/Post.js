2f727138281af12110780ef4d0ce20e471bc4211
Sebosun <sebos@protonmail.com>
2021-07-16 14:30:48 +0200

    cleaned up leftover code

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,23 +3,6 @@ import "./Post.css";
 import Image from "./UI/Image";
 
 function Post(props) {
-  //TODO CLEAN THIS
-  const [imgSize, setImgSize] = useState({
-    maxHeight: "15vh",
-  });
-
-  const handleImageSize = () => {
-    if (imgSize.maxHeight === "15vh") {
-      setImgSize({
-        maxHeight: "45vh",
-      });
-    } else {
-      setImgSize({
-        maxHeight: "15vh",
-      });
-    }
-  };
-
   return (
     <div className="Post">
       <div className="postContainer">
