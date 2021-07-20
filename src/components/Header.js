f369d142995e5774c5410c6f571c243ae7ff8bd6
Sebosun <sebos@protonmail.com>
2021-07-21 00:46:09 +0200

    clicking on the header will return to main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header(props) {
   return (
     <div className="header">
-      <h1>{props.chan}</h1>
+      <Link to="/">
+        <h1>{props.chan}</h1>
+      </Link>
       <p>{props.desc}</p>
     </div>
   );
