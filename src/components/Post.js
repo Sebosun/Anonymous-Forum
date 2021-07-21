5d6bd8db46310fd638a13fd6f2ec0d23f49dea0a
Sebosun <sebos@protonmail.com>
2021-07-21 14:14:06 +0200

    can hide posts now

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,26 +1,46 @@
 import "./Post.css";
+import { useState } from "react";
 import Image from "./UI/Image";
+import HideElement from "./UI/HideElement";
 
 function Post(props) {
+  const [postVisible, setPostVisible] = useState(true);
+  const [textOfHidePost, setTextOfHidePost] = useState("close");
+
+  function onHideThread() {
+    setPostVisible((prevState) => !prevState);
+    setTextOfHidePost((prevState) => {
+      if (prevState == "close") {
+        return props.user ? props.user : "Anyonymous";
+      } else {
+        return "close";
+      }
+    });
+  }
   return (
-    <div className="Post">
-      <div className="postContainer">
-        <Image src={props.image} />
-        <div className="posterInfo">
-          <div id="user">Anonymous</div>
-          <div>{props.title}</div>
-          {props.time && (
-            <div>
-              {props.time.toDate().toDateString() +
-                " " +
-                props.time.toDate().toLocaleTimeString()}
+    <main>
+      <HideElement text={textOfHidePost} onClick={onHideThread} />
+      {postVisible && (
+        <div className="Post">
+          <div className="postContainer">
+            <Image src={props.image} />
+            <div className="posterInfo">
+              <div id="user">Anonymous</div>
+              <div>{props.title}</div>
+              {props.time && (
+                <div>
+                  {props.time.toDate().toDateString() +
+                    " " +
+                    props.time.toDate().toLocaleTimeString()}
+                </div>
+              )}
+              <div className="postNo">No. {props.postNo}</div>
             </div>
-          )}
-          <div className="postNo">No. {props.postNo}</div>
+            <p className="postText">{props.text}</p>
+          </div>
         </div>
-        <p className="postText">{props.text}</p>
-      </div>
-    </div>
+      )}
+    </main>
   );
 }
 
