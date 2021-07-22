29ca61fdb43bc488691ff07b61c8051b3dcd98b2
Sebosun <sebos@protonmail.com>
2021-07-22 14:08:10 +0200

    icon + fixing the text breaking up on new lines

--- src/components/UI/ExpandThread.js
+++ src/components/UI/ExpandThread.js
@@ -1,9 +1,10 @@
 import styles from "./ExpandThread.module.css";
+import { BsArrowsExpand, BsBoxArrowInDown } from "react-icons/bs";
 
 const ExpandThread = (props) => {
   return (
     <div className={styles.expandThread}>
-      <div>Icon :^)</div>
+      <BsBoxArrowInDown size="1.5em" />
       <div>{props.postNo - 3} posts omitted.</div>
       <div onClick={props.onClick} className={styles.expand}>
         Click to expand.
